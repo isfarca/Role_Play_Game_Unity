28371fea74840cdc25e75e506be6e9b293196699
Fethi Isfarca <fisfarca@googlemail.com>
2019-10-04 11:13:39 +0200

    Player movement
    Walk, Run, Crouch and Respawn point added.

---
+++ Role_Play_Game/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+
+public class PlayerController : MonoBehaviour 
+{
+    // Components.
+    private Rigidbody physics;
+    
+    // Attributes.
+    [SerializeField] private float speed;
+
+    /// <summary>
+    /// Initializing.
+    /// </summary>
+    private void Start()
+    {
+        physics = GetComponent<Rigidbody>();
+    }
+
+    /// <summary>
+    /// Movement.
+    /// </summary>
+    private void FixedUpdate()
+    {
+        // Get values.
+        var moveHorizontal = Input.GetAxis("Horizontal");
+        var moveVertical = Input.GetAxis("Vertical");
+        
+        // Write movement.
+        var movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+
+        // Increase speed for running on shift click.
+        speed = Input.GetKey(KeyCode.LeftShift) ? 20 : 10;
+
+        // Is space clicked.
+        if (Input.GetKey(KeyCode.Space)) // Yes.
+        {
+            // Crouch.
+            var playerTransform = transform;
+            
+            // Set default rotation and freeze rotation.
+            playerTransform.rotation = Quaternion.identity;
+            physics.constraints = RigidbodyConstraints.FreezeRotation;
+
+            // Add crouch speed and crouch player.
+            var crouchSpeed = speed / 100;
+            playerTransform.position += new Vector3(moveHorizontal * crouchSpeed * Time.deltaTime, 0.0f, moveVertical * crouchSpeed * Time.deltaTime);
+        }
+        else // No.
+        {
+            // Disable physics freezes.
+            physics.constraints = RigidbodyConstraints.None;
+            
+            // Move.
+            physics.AddForce(movement * speed);
+        }
+    }
+}
---
+++ Role_Play_Game/Assets/Scripts/Respawn.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public class Respawn : MonoBehaviour
+{
+    // Important objects.
+    [SerializeField] private Transform player;
+    [SerializeField] private Transform respawnPoint;
+    
+    /// <summary>
+    /// Respawn player.
+    /// </summary>
+    /// <param name="other">Player.</param>
+    private void OnCollisionEnter(Collision other)
+    {
+        // Is the other collided object a player?
+        if (other.transform == player) // Yes.
+        {
+            // Clear physics speed.
+            player.GetComponent<Rigidbody>().Sleep();
+
+            // Respawn player.
+            player.position = respawnPoint.position;
+            player.rotation = respawnPoint.rotation;
+        }
+    }
+}
