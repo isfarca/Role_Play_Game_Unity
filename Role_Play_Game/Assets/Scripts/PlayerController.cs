47ea5adc9490c635b1d7abf14c026ea260699c52
Fethi Isfarca <fisfarca@googlemail.com>
2019-10-04 13:03:27 +0200

    Player stamina
    Stamina logic added.

--- Role_Play_Game/Assets/Scripts/PlayerController.cs
+++ Role_Play_Game/Assets/Scripts/PlayerController.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 
 public class PlayerController : MonoBehaviour 
 {
@@ -7,6 +8,8 @@ public class PlayerController : MonoBehaviour
     
     // Attributes.
     [SerializeField] private float speed;
+    [SerializeField] private float stamina;
+    private bool isBreathless;
 
     /// <summary>
     /// Initializing.
@@ -16,23 +19,59 @@ public class PlayerController : MonoBehaviour
         physics = GetComponent<Rigidbody>();
     }
 
+    private void Update()
+    {
+        // Output stamina.
+        Debug.Log("Stamina: " + stamina);
+
+        // Player moved?
+        if (Math.Abs(Input.GetAxis("Horizontal")) <= 0.0f && Math.Abs(Input.GetAxis("Vertical")) <= 0.0f) // No.
+        {
+            // Have stamina the maximum value?
+            if (stamina < 10.0f) // No.
+            {
+                // Get more stamina.
+                stamina += speed / 1000.0f;
+            }
+        }
+        else // Yes.
+        {
+            // Have stamina the minimum value?
+            if (stamina > 0.0f)
+            {
+                // Increase stamina with consideration of crouch.
+                stamina -= Input.GetKey(KeyCode.Space) ? speed / 1000000.0f : speed / 1000.0f;
+            }
+        }
+
+        // Can the player move or not?
+        isBreathless = stamina <= 0.0f;
+    }
+
     /// <summary>
     /// Movement.
     /// </summary>
     private void FixedUpdate()
     {
+        // No stamina anymore?
+        if (isBreathless) // Yes.
+        {
+            // Disable movement.
+            return;
+        }
+        
         // Get values.
         var moveHorizontal = Input.GetAxis("Horizontal");
         var moveVertical = Input.GetAxis("Vertical");
-        
+
         // Write movement.
         var movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
 
-        // Increase speed for running on shift click.
+        // Left shift clicked?
         speed = Input.GetKey(KeyCode.LeftShift) ? 20 : 10;
 
         // Is space clicked.
-        if (Input.GetKey(KeyCode.Space)) // Yes.
+        if (Input.GetKey(KeyCode.Space)) // Yes. [CROUCH]
         {
             // Crouch.
             var playerTransform = transform;
@@ -45,7 +84,7 @@ public class PlayerController : MonoBehaviour
             var crouchSpeed = speed / 100;
             playerTransform.position += new Vector3(moveHorizontal * crouchSpeed * Time.deltaTime, 0.0f, moveVertical * crouchSpeed * Time.deltaTime);
         }
-        else // No.
+        else // No. [WALK | RUN]
         {
             // Disable physics freezes.
             physics.constraints = RigidbodyConstraints.None;
